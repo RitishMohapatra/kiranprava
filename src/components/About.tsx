30dabaa313807afb36b359d1e1cf1346ec23a7b5
RitishMohapatra <ritish.mohapatra18@nmims.edu.in>
2023-04-04 01:12:36 +0530

    hjuiokjhui

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
   // const { logo, name: companyName } = company;
   // const { socialMedia, sections } = about;
 
-  return (
+return (
     <footer
       className="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left"
       id="footer"
@@ -180,9 +180,9 @@ const About = () => {
                 <path
                   fill-rule="evenodd"
                   d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
-                  clip-rule="evenodd"
+  clip-rule="evenodd"
                 />
-              </svg>
+     </svg>
               +91 89843 65125
             </p>
           </div>
