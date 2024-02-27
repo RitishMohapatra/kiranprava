bb18dfea6579df102a824a6948d2c0483663cbae
RitishMohapatra <ritish.mohapatra18@nmims.edu.in>
2024-02-27 21:34:29 +0530

    images added

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -166,7 +166,7 @@ return (
         </div>
       </div>
       <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
-        <span>© 2023 Copyright:</span>
+        <span>© 2024 Copyright:</span>
         <a
           className="font-semibold text-neutral-600 dark:text-neutral-400"
           href="https://ritishm.netlify.app/" target="_blank"
